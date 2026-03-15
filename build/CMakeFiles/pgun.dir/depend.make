11680422779cf7fbf7de3872fb7164d7e7ed43f0
PRIM2 <ferran.gb.04@gmail.com>
2026-03-15 20:37:10 +0100

    Adding lines

--- build/CMakeFiles/pgun.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pgun.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/pgun.dir/src/ActionInitialization.cc.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/pgun.dir/src/ActionInitialization.cc.o: \
- /home/ferrangb/Desktop/rpc-simulation-geant4/src/ActionInitialization.cc \
- /usr/include/stdc-predef.h \
- /home/ferrangb/Desktop/rpc-simulation-geant4/include/ActionInitialization.hh \
- /home/ferrangb/Desktop/entornos/Geant4/Binarios/include/Geant4/G4VUserActionInitialization.hh \
- /home/ferrangb/Desktop/rpc-simulation-geant4/include/PrimaryGeneratorAction.hh \
- /home/ferrangb/Desktop/entornos/Geant4/Binarios/include/Geant4/G4VUserPrimaryGeneratorAction.hh
